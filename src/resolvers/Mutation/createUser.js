a6d1533835a383d7bd8afcf9c553d38adbb5f6dd
Alex Nitta <a.j.nitta@gmail.com>
2019-10-31 11:52:20 -0700

    Update sample mutation and query

--- src/resolvers/Mutation/createUser.js
+++ src/resolvers/Mutation/createUser.js
@@ -1,10 +1,13 @@
 async function createUser(parentValue, args, context) {
-  return {
-    id: 2,
-    firstName: "Test2",
-    lastName: "User2",
-    emailAddress: "test2user2@example.com"
-  };
-}
+                                                          // TODO implement a real mutation here, using the context to access a database
+                                                          return {
+                                                              id: 3,
+                                                              firstName:
+                                                                  'Test3',
+                                                              lastName: 'User3',
+                                                              emailAddress:
+                                                                  'test3user3@example.com',
+                                                          };
+                                                      }
 
 export { createUser };
